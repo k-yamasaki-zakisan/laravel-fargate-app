7736c6427814ce221ce8be0bf8e924e956bfb608
kazuyoshi-tech <kazuyoshi.kyushu@gmail.com>
2022-05-21 11:34:59 +0900

    github action の初期設定導入

--- laravel-fargate-infra/envs/prod/app/foobar/ecr.tf
+++ laravel-fargate-infra/envs/prod/app/foobar/ecr.tf
@@ -31,5 +31,11 @@
 module "nginx" {
   source = "../../../../modules/ecr"
 
-  name = "example-prod-foobar-nginx"
+  name = "${local.name_prefix}-${local.service_name}-nginx"
+}
+
+module "php" {
+  source = "../../../../modules/ecr"
+
+  name = "${local.name_prefix}-${local.service_name}-php"
 }
